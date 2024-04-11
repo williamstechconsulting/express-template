689635e437f427c2ea765c333626f36ba66b74dc
williamstechconsulting <ted@techwilliams.com>
2024-04-10 19:31:32 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# WTC Express.js Starter Template
+
+- Express.js
+- Pug Templating
+- Dart-Sass
+- dotenv
+
+## Get Started
+
+```bash
+git clone https://github.com/williamstechconsulting/express-template.git
+cd express-template
+npm start
+```
+
+## Customization
+Custom script for compiling Sass to CSS in `./bin`. Default `node-sass` package is deprecated and causes bugs in modern environments. This template uses [Dart-Sass](https://sass-lang.com/dart-sass/).
+
+[This](https://dev.to/ccreusat/migrating-from-node-sass-to-sass-dart-sass-with-npm-3g3c) is a simple guide on migrating from `node-sass` to `sass` in node.js.
--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var sassMiddleware = require('node-sass-middleware');
+const { compileCss } = require('./bin/compileCss');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,12 +19,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
-}));
+const sassOptions = {
+  src: path.join(__dirname, 'public/stylesheets'),
+  dest: path.join(__dirname, 'public/stylesheets')
+};
+compileCss(sassOptions);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
